86c389164719e078679efa94364ecf10ebd52c63
Dylan Vinesett <dvinesett@gmail.com>
2015-02-03 04:24:28 -0500

    added interpreter directive line and a comment

--- rspec_reinstaller.sh
+++ rspec_reinstaller.sh
@@ -1,4 +1,6 @@
+#!/bin/sh
 echo "~~~ Uninstalling all versions of RSpec as well as related excecutables"
+# parses for all known rspec dependencies and force uninstalls them (including related executables)
 for r in $(gem list | grep rspec | cut -d ' ' -f 1); do
   gem uninstall -xa --force $r
 done
